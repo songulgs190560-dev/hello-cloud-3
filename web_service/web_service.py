724b317de9a9b523406386698f51fcadb1ea33d5
songulgs190560-dev <songulgs190560@gmail.com>
2025-10-30 14:52:43 +0300

    Update web_service.py

--- web_service/web_service.py
+++ web_service/web_service.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_URL = https://hello-cloud-3-78hi.onrender.com/
+API_URL = "https://hello-cloud-3-78hi.onrender.com/ziyaretçiler
 
 HTML = """
 <!doctype html>
