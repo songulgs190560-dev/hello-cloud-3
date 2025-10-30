5b7e9e98a5a80df69a5dc56ed1886ad34b7b2061
songulgs190560-dev <songulgs190560@gmail.com>
2025-10-30 14:18:12 +0300

    Update web_service.py

--- web_service/web_service.py
+++ web_service/web_service.py
@@ -3,30 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_URL = "https://hello-cloud4.onrender.com"
-
-HTML = """
-<!doctype html>
-<html>
-<head>
-  <title>Mikro Hizmetli Selam!</title>
-  <style>
-     body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
-     h1 { color: #333; }
-     input { padding: 10px; font-size: 16px; }
-     button { padding: 10px 15px; background: #4CAF50; color: white; border: none; border-radius: 6px; cursor: pointer; }
-     li { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
-  </style>
-</head>
-<body>
-   <h
-
-from flask import Flask, render_template_string, request, redirect
-import requests
-
-app = Flask(__name__)
-
-API_URL = "https://hello-cloud4.onrender.com"
+API_URL = "https://hello-cloud4.onrender.c
 
 HTML = """
 <!doctype html>
